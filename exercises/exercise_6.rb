fe15691c68d7966489de413c888f915bcb14501f
OliviaWong-dev <olivia.wy.wong@gmail.com>
2022-01-04 00:54:56 +0000

    create association btw employees and stores table and create sample employees data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Hello", last_name: "Mimi", hourly_rate: 100)
+@store1.employees.create(first_name: "Ray", last_name: "Fong", hourly_rate: 50)
+@store1.employees.create(first_name: "Chimunk", last_name: "Don", hourly_rate: 65)
+@store2.employees.create(first_name: "Fire", last_name: "Water", hourly_rate: 90)
+@store2.employees.create(first_name: "Pika", last_name: "Chu", hourly_rate: 200)
